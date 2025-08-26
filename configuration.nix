462c3658fb3d540dfcb90fc622b9654b9166d43e
Tim de Klijn <timdeklijn@gmail.com>
2025-08-26 12:32:15 +0200

    small cleanup

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
-     <nixos-hardware/framework/13-inch/7040-amd>
+     <nixos-hardware/framework/13-inch/7040-amd> # Framework specific hardware drivers
      ./hardware-configuration.nix
     ];
 
@@ -17,10 +13,6 @@
 
   networking.hostName = "fw13"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -96,27 +88,31 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tim = {
     isNormalUser = true;
     description = "Tim de Klijn";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
-	direnv
-	eza
-	fzf
-	git
-	git-lfs
-	kitty
-	starship
-	tmux
-	vim
-	zoxide
+	    direnv
+	    eza
+	    fzf
+	    git
+	    git-lfs
+	    kitty
+	    starship
+	    tmux
+	    vim
+      wget
+	    zoxide
 
-	dropbox
-	obsidian
-	signal-desktop
-	vscode-fhs
+	    dropbox
+	    obsidian
+	    signal-desktop
+	    vscode-fhs
     ];
     shell = pkgs.zsh;
   };
@@ -151,37 +147,25 @@
 
   programs.starship.enable = true;
 
+  # This is required to get the right drivers fro my framework laptop
   services.fwupd.enable = true;
   hardware.framework.amd-7040.preventWakeOnAC = true;
 
   # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
 
   environment.systemPackages = with pkgs; [
-	displaylink
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    # Follow this:
+    # https://wiki.nixos.org/wiki/Displaylink
+    # there is a manual step to download the package in there.
+    displaylink
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # remove old generations
   nix.gc = {
   	automatic = true;
   	dates = "daily";
