29e3db48ec72ca6f4fa55e9578e7570ac5c0a644
Tim de Klijn <timdeklijn@gmail.com>
2025-09-01 12:59:08 +0200

    generation 48

--- flake.nix
+++ flake.nix
@@ -3,17 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    # Used for user packages and dotfiles
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs"; # Use system packages list where available
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+    in
     {
       # Describe my system
-      nixosConfigurations.fw13 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
+      nixosConfigurations = {
+        fw13 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+          ];
+        };
       };
+
+      homeConfigurations = {
+        "tim@fw13" = home-manager.lib.HomeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ "home.nix" ];
+        };
+      };
+
     };
 }
