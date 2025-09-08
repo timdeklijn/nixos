8915242d2040f4d7b982dfeed6e841d410439b5f
Tim de Klijn <timdeklijn@gmail.com>
2025-09-08 20:37:28 +0200

    generation 106

--- home.nix
+++ home.nix
@@ -64,13 +64,16 @@
       num-workspaces = 4;
     };
     "org/gnome/desktop/wm/keybindings" = {
-      # Switch to a specific workspace (Super+1..6)
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+
       switch-to-workspace-1 = [ "<Super>1" ];
       switch-to-workspace-2 = [ "<Super>2" ];
       switch-to-workspace-3 = [ "<Super>3" ];
       switch-to-workspace-4 = [ "<Super>4" ];
 
-      # Move the active window to a specific workspace (Shift+Super+1..6)
       move-to-workspace-1 = [ "<Shift><Super>1" ];
       move-to-workspace-2 = [ "<Shift><Super>2" ];
       move-to-workspace-3 = [ "<Shift><Super>3" ];
@@ -90,6 +93,14 @@
     };
     "org/gnome/desktop/sound" = {
       event-sounds = false;
+    };
+    "org/gnome/shell/extensions/dash-to-panel" = {
+      panel-position = "TOP"; # TOP, LEFT, RIGHT, BOTTOM
+      panel-size = 36; # pixels
+      show-show-apps-button = true;
+      intellihide = true; # auto-hide when windows overlap
+      isolate-monitors = false; # single unified panel
+      show-appmenu = false; # hide the old app menu
     };
   };
 
