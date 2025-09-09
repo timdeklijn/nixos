52c5e25586d88097c374c7c20f04d7528e819f8d
Tim de Klijn <timdeklijn@gmail.com>
2025-09-09 11:50:26 +0200

    generation 130

--- home.nix
+++ home.nix
@@ -128,7 +128,7 @@
     # These settings are simply copied from my kitty config.
     settings = {
       # Fonts
-      font_family = "MonaspiceKr Nerd Font Mono";
+      font_family = "MonaspiceAr Nerd Font Mono";
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
