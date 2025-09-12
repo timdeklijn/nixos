e0d7bcbf96148ded5ee17b58c6890449b366ac8b
Tim de Klijn <timdeklijn@gmail.com>
2025-09-12 20:49:31 +0200

    generation 164

--- applications/vscode.nix
+++ applications/vscode.nix
@@ -91,6 +91,8 @@
           "(builtins.getFlake \"/path/to/flake.nix\").nixosConfigurations.<name>.options.home-manager.users.type.getSubOptions []";
         "nix.serverSettings.options.nixos.expr" =
           "(builtins.getFlake \"/path/to/flake.nix\").nixosConfigurations.<name>.options";
+        "terminal.integrated.defaultProfile.linux" = "null";
+        "terminal.integrated.shell.linux" = "/run/current-system/sw/bin/bash";
       };
       keybindings = [
         {
