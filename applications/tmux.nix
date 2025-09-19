457856c9b555a532fdcffb144e0c3b2d5453a780
Tim de Klijn <timdeklijn@gmail.com>
2025-09-19 06:47:29 +0200

    generation 212

--- applications/tmux.nix
+++ applications/tmux.nix
@@ -20,7 +20,8 @@
           set -g @dracula-show-powerline true
 
           set -g @dracula-show-battery true
-          set -g @dracula-show-battery-status true
+          # set -g @dracula-battery-label "♥ "
+          # set -g @dracula-show-battery-status true
 
           set -g @dracula-cpu-display-load true
           set -g @dracula-cpu-usage-label " "
