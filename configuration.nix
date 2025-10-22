362799fd00630e148ea05b155d760397f610a165
Tim de Klijn <timdeklijn@gmail.com>
2025-10-22 06:02:50 +0200

    generation 300

--- configuration.nix
+++ configuration.nix
@@ -85,6 +85,7 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  programs.hyprland.enable = true;
 
   systemd.services.displaylink-server = {
     enable = true;
