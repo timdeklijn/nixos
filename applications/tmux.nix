d940033330e18654229236194b50e99ea412cf35
Tim de Klijn <timdeklijn@gmail.com>
2025-11-12 06:16:45 +0100

    generation 344

--- applications/tmux.nix
+++ applications/tmux.nix
@@ -11,30 +11,6 @@
     sensibleOnTop = true;
     escapeTime = 1;
     baseIndex = 1;
-    plugins = [
-      pkgs.tmuxPlugins.vim-tmux-navigator
-      {
-        plugin = pkgs.tmuxPlugins.dracula;
-        extraConfig = ''
-          set -g @dracula-refresh-rate 10
-          set -g @dracula-show-powerline true
-
-          set -g @dracula-show-battery true
-
-          set -g @dracula-show-network false
-
-          set -g @dracula-show-cpu true
-
-          set -g @dracula-cpu-display-load true
-          set -g @dracula-cpu-usage-label " "
-
-          set -g @dracula-show-weather false
-          # set -g @dracula-show-fahrenheit false
-          # set -g @dracula-show-location false
-
-        '';
-      }
-    ];
     extraConfig = ''
       set-option -g status-position bottom
 
